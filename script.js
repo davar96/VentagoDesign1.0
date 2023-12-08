eba7d43433048c6706b18f63f4cd4c12f01d9d2f
David Arustamyan <david.arustamyan.96@gmail.com>
2023-12-08 03:11:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//copy menu for mobile
+function copyMenu() {
+  //copy inside .listings-cat to listings
+  const listingsCategory = document.querySelector(".listings-cat");
+  const listingsPlace = document.querySelector(".listings");
+
+  listingsPlace.innerHTML = listingsCategory.innerHTML;
+
+  //copy inside nav to nav
+  const mainNav = document.querySelector(".header-nav nav");
+  const navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  //copy .header-top .wrapper to .theTop-nav
+  const topNav = document.querySelector(".header-top .wrapper");
+  const topPlace = document.querySelector(".off-canvas .theTop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
+
+//show mobile menu
+const menuButton = document.querySelector(".trigger"),
+  closeButton = document.querySelector(".t-close"),
+  addClass = document.querySelector(".site");
+
+menuButton.addEventListener("click", function () {
+  addClass.classList.toggle("showMenu");
+});
+closeButton.addEventListener("click", function () {
+  addClass.classList.remove("showMenu");
+});
+
+//show sub menu on mobile
+const subMenu = document.querySelectorAll(".has-child .icon-small");
+subMenu.forEach((menu) => menu.addEventListener("click", toggle));
+
+function toggle(e) {
+  e.preventDefault();
+  subMenu.forEach((item) =>
+    item != this ? item.closest(".has-child").classList.remove("expand") : null
+  );
+  if (this.closest(".has-child").classList != "expand");
+  this.closest(".has-child").classList.toggle("expand");
+}
