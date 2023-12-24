528ede0e2adfe9630f56bdcbbdb87fe57003428c
Davar96 <david.arustam96@gmail.com>
2023-12-25 00:35:44 +0100

    slider integration start

--- script.js
+++ script.js
@@ -42,3 +42,12 @@ function toggle(e) {
   if (this.closest(".has-child").classList != "expand");
   this.closest(".has-child").classList.toggle("expand");
 }
+
+//Slider
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
